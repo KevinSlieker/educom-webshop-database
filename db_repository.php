6861185ac1705ef2b9d387e6bbe180aa8123119e
Kevin Slieker <kevin.slieker.10@gmail.com>
2022-10-17 17:06:53 +0200

    3.1 Issue #2

--- db_repository.php
+++ db_repository.php
@@ -14,7 +14,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 
     if (!$conn) {
-     die("Connection failed: " . mysqli_connect_error());
+        throw new Exception("Cannot connect with the database." . mysqli_connect_error());
     }
     return $conn;
 }
@@ -25,74 +25,42 @@ function closeDatabase($conn){
 
 function saveUser($email,$name,$password) {
     $conn = connectDatabase();
+    try {
+        $name = mysqli_real_escape_string($conn, $name);
+        $email = mysqli_real_escape_string($conn, $email);
+        $password = mysqli_real_escape_string($conn, $password);        
 
-    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+        $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
     
-    if (!mysqli_query($conn, $sql)) {
-          echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        if (!mysqli_query($conn, $sql)) {
+         throw new Exception("Query failed, SQL: " . $sql . "Error: " . mysqli_error($conn));
+        }
     }
-
+ // waar moet de catch nu en hoe zit het dan met all mijn errers voor fout email en password?
+    finally {
     closeDatabase($conn);
+    }
 } 
 
 function findUserByEmail($email){
     $conn = connectDatabase();
     $user = NULL;
-    $sql = "SELECT user_ID, name, email, password FROM users WHERE email = '$email'";
-    $result = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($result) > 0 ){
+    try {
+        $email = mysqli_real_escape_string($conn, $email);
+        $sql = "SELECT user_ID, name, email, password FROM users WHERE email = '$email'";
+        $result = mysqli_query($conn, $sql);
+        if ($result == false) {
+        throw new Exception("Query failed, SQL: " . $sql . "Error: " . mysqli_error($conn));
+        }
+        if (mysqli_num_rows($result) > 0 ){
         $user = mysqli_fetch_assoc($result);
+        }
+        return $user;
     }
-
+    finally {
     closeDatabase($conn);
-    return $user;
-
-/*    
-	$file = fopen('users/users.txt', 'r');
-	$user = NULL;
-	$line = fgets($file);
-
-    while (!feof($file)) {
-        $line = fgets($file);
-        $explode = explode('|', $line);
-        if ($explode[0] == $email) {
-            $user = array("email" => $explode[0], "name"=> $explode[1], "password"=> $explode[2]);
-        }
     }
-    fclose($file);
-    return $user;
-*/
-}
-/*
-
-
-
-
-    $sql = "SELECT user_ID, name, email FROM users";
-    $result = mysqli_query($conn, $sql);
     
-    if (mysqli_num_rows($result) > 0) {
-        // output data of each row
-        while($row = mysqli_fetch_assoc($result)) {
-            echo "id: " . $row["user_ID"]. " - Name: " . $row["name"]. " " . $row["email"]. "<br>";
-        }
-    } else {
-        echo "0 results";
-    }
 
-
-
-
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
-
-if (mysqli_query($conn, $sql)) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
-
-mysqli_close($conn);
-*/
-
 ?>
