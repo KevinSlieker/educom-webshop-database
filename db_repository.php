f0ed6b3f8064c3e39dacc1fef9648431951c6a7b
Kevin Slieker <kevin.slieker.10@gmail.com>
2022-10-17 14:49:28 +0200

    3.1 Issue #2

--- db_repository.php
+++ db_repository.php
@@ -1,5 +1,6 @@
 <?php
 
+function connectDatabase(){
 
 $servername = "localhost";
 $username = "Kevins_WebShopUser";
@@ -15,16 +16,57 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
     if (!$conn) {
      die("Connection failed: " . mysqli_connect_error());
     }
-    echo "Connected";
-
-
-$sql = "INSERT INTO users (name, email, password) VALUES ('John Doe', 'john@example.com', 'John123')";
-    
-if (mysqli_query($conn, $sql)) {
-      echo "New record created successfully";
-    } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    return $conn;
 }
+
+function closeDatabase($conn){
+    mysqli_close($conn);
+}
+
+function saveUser($email,$name,$password) {
+    $conn = connectDatabase();
+
+    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+    
+    if (!mysqli_query($conn, $sql)) {
+          echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    closeDatabase($conn);
+} 
+
+function findUserByEmail($email){
+    $conn = connectDatabase();
+    $user = NULL;
+    $sql = "SELECT user_ID, name, email, password FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0 ){
+        $user = mysqli_fetch_assoc($result);
+    }
+
+    closeDatabase($conn);
+    return $user;
+
+/*    
+	$file = fopen('users/users.txt', 'r');
+	$user = NULL;
+	$line = fgets($file);
+
+    while (!feof($file)) {
+        $line = fgets($file);
+        $explode = explode('|', $line);
+        if ($explode[0] == $email) {
+            $user = array("email" => $explode[0], "name"=> $explode[1], "password"=> $explode[2]);
+        }
+    }
+    fclose($file);
+    return $user;
+*/
+}
+/*
+
+
+
 
     $sql = "SELECT user_ID, name, email FROM users";
     $result = mysqli_query($conn, $sql);
@@ -37,12 +79,6 @@ if (mysqli_query($conn, $sql)) {
     } else {
         echo "0 results";
     }
-
-mysqli_close($conn);
-
-/*
-
-
 
 
 
