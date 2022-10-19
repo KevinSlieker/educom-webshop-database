691ca8262258bdd57b5a45685e120ca06bb04367
Kevin Slieker <kevin.slieker.10@gmail.com>
2022-10-19 14:51:12 +0200

    3.2 Issue #4 database

--- database/kevins_webshop.sql
+++ database/kevins_webshop.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Gegenereerd op: 18 okt 2022 om 13:02
+-- Gegenereerd op: 19 okt 2022 om 12:50
 -- Serverversie: 10.4.25-MariaDB
--- PHP-versie: 8.1.9
+-- PHP-versie: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,10 +24,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabelstructuur voor tabel `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(30) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `filename_img` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `filename_img`) VALUES
+(1, 'Island', 'Een basic full art island van de Zendikar set.', '€1,00', 'island.jpg'),
+(2, 'Forest', 'Een basic full art forest van de Zendikar set.', '€1,00', 'forest.jpg'),
+(3, 'Mountain', 'Een basic full art mountain van de Zendikar set.', '€1,00', 'mountain.jpg'),
+(4, 'Plains', 'Een basic full art plains van de Zendikar set.', '€1,00', 'plains.jpg'),
+(5, 'Swamp', 'Een basic full art swamp van de Zendikar set.', '€1,00', 'swamp.jpg');
+
+-- --------------------------------------------------------
+
+--
 -- Tabelstructuur voor tabel `users`
 --
 
-CREATE TABLE IF NOT EXISTS  `users` (
+CREATE TABLE `users` (
   `user_ID` int(30) NOT NULL,
   `name` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -39,7 +64,7 @@ CREATE TABLE IF NOT EXISTS  `users` (
 --
 
 INSERT INTO `users` (`user_ID`, `name`, `email`, `password`) VALUES
-(1, 'Test', 'test@test.com', '123');
+(10, 'test', 'test@test.com', '123');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -59,7 +84,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
