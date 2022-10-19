e4e7a866a06028b1f4e058410a8239ec48601aea
Kevin Slieker <kevin.slieker.10@gmail.com>
2022-10-19 14:42:17 +0200

    3.2 Issue #4

--- detail.php
+++ detail.php
@@ -1,14 +1,19 @@
 <?php
 
 function showDetailHead($data){
-    echo "";
+    echo $data['product']['name'];
 }
 
 function showDetailHeader($data) {
-    echo "";
+    echo $data['product']['name'];
 }
 
 function showDetailContent($data) {
-
+    echo '<div class="detail">';
+    echo '<h2>' . $data['product']['name'] . '</h2>';
+    echo '<img src="Images/' . $data['product']['filename_img'] . '" alt="' . $data['product']['name'] . '" width="150 height="300"><br><br>';
+    echo '<p4>Bechrijving: ' . $data['product']['description'] . '</p4><br><br>';
+    echo '<p5>' . $data['product']['price'] . '</p5></a>';
+    echo '</div>';  
 }
 ?>
--- webshop.php
+++ webshop.php
@@ -11,10 +11,11 @@ function showWebshopHeader() {
 function showWebshopContent($data) {
     echo '<div class="products">';
     foreach($data['products'] as $product) {
-    echo '<a href="index.php?page=detail&id=' . $product['id'] . '">';
+    echo '<div class="product"><a href="index.php?page=detail&id=' . $product['id'] . '">';
     echo '<h2>' . $product['name'] . '</h2>';
     echo '<img src="Images/' . $product['filename_img'] . '" alt="' . $product['name'] . '" width="60" height="80"><br>';
-    echo '<p1>' . $product['price'] . '</p1></a>';
+    echo '<p4>Id: ' . $product['id'] . '</p4><br>';
+    echo '<p5>Prijs: ' . $product['price'] . '</p5></a></div>'. PHP_EOL;
     }
     echo '</div>';  
 }
